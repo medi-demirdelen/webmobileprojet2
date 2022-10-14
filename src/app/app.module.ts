2f0aca5a419b7b4d9d1e949bc51eca790ac9422a
medi.demirdelen <medi.demirdelen@gmail.com>
2022-10-14 15:10:52 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { MapComponent } from './map/map.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    LeafletModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
